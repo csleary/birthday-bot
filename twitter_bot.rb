31be5c19268c50fdd11899768c60efa96b5b3876
Chris <csleary@gmail.com>
2017-06-23 13:19:11 +0100

    Cleaned up the quotes, magic comment, now uses env vars.

--- twitter_bot.rb
+++ twitter_bot.rb
@@ -1,15 +1,12 @@
-#!/usr/bin/env ruby -wKU
+# frozen_string_literal: true
 
 require 'twitter'
-require 'yaml'
-
-key = YAML.load_file(File.join(__dir__, 'key.yml'))
 
 tweet = Twitter::REST::Client.new do |config|
-  config.consumer_key        = key['twitter']['consumer_key']
-  config.consumer_secret     = key['twitter']['consumer_secret']
-  config.access_token        = key['twitter']['access_token']
-  config.access_token_secret = key['twitter']['access_token_secret']
+  config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+  config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+  config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
 end
 
 today = Date.today
@@ -18,42 +15,41 @@ case today.strftime('%d-%m')
 when '29-11'
   album = "💿 'A Midsummer Nice Dream' (Toytronic)"
   release_year = 2004
-  url = "https://ochre.bandcamp.com/album/a-midsummer-nice-dream"
+  url = 'https://ochre.bandcamp.com/album/a-midsummer-nice-dream'
 when '10-07'
   album = "💿 'Lemodie' (Benbecula)"
   release_year = 2006
-  url = "https://ochre.bandcamp.com/album/lemodie"
+  url = 'https://ochre.bandcamp.com/album/lemodie'
 when '20-07'
   album = "💿 'Like Dust of the Balance' (Benbecula)"
   release_year = 2009
-  url = "https://ochre.bandcamp.com/album/like-dust-of-the-balance"
+  url = 'https://ochre.bandcamp.com/album/like-dust-of-the-balance'
 when '01-08'
   album = "🎵 'Early Learning'"
   release_year = 2011
-  url = "https://ochre.bandcamp.com/album/early-learning"
+  url = 'https://ochre.bandcamp.com/album/early-learning'
 when '21-01'
   album = "🎵 'National Ignition' (Aura Materia)"
   release_year = 2013
-  url = "https://ochre.bandcamp.com/album/national-ignition"
+  url = 'https://ochre.bandcamp.com/album/national-ignition'
 when '13-01'
   album = "💿 'Isolette' (Shipwrec)"
   release_year = 2015
-  url = "https://ochre.bandcamp.com/album/isolette"
+  url = 'https://ochre.bandcamp.com/album/isolette'
 when '24-02'
   album = "🎵 'Beyond the Outer Loop' (Aura Materia)"
   release_year = 2017
-  url = "https://ochre.bandcamp.com/album/beyond-the-outer-loop"
+  url = 'https://ochre.bandcamp.com/album/beyond-the-outer-loop'
 end
 
 unless release_year.nil?
-
   age = today.year - release_year
-  if age < 2
-    age = "a year"
-  else
-    age = "#{age} years"
-  end
+  age =
+    if age < 2
+      'a year'
+    else
+      "#{age} years"
+    end
 
-  tweet.update("🤖 #{album} was released on this day, #{age} ago! 🎂 🎉 #{url}")
-
+  tweet.update("🤖💬 #{album} was released on this day, #{age} ago! 🎂 🎉 #{url}")
 end
